4657b7b2b0deab88af3497fa790fbf6b05a38ade
Chris Jerrett <CJerrett18@stjohnsprep.org>
2017-09-21 15:19:04 -0400

    Fixed Comments

--- src/slew.c
+++ src/slew.c
@@ -1,31 +1,14 @@
 #include "slew.h"
 #include "log.h"
-/**
-* @brief mutex to protect the data in the array of speeds from being read or written to simultaneously.
-* @author Chris Jerrett
-* @date 9/14/17
-**/
+
 static Mutex mutex;
 
-/**
-* @brief Array of motor speed values to set the motors to.
-* @author Chris Jerrett
-* @date 9/14/17
-**/
+
 static signed char *motors_set_speeds = NULL;
 
-/**
-* @brief Task that will handle updating the motors on a routine period.
-* @author Chris Jerrett
-* @date 9/14/17
-**/
 static TaskHandle slew = NULL; //TaskHandle is of type void*
 
-/**
-* @brief Boolean indicating whether or not the slew rate controller has been initialized
-* @author Chris Jerrett
-* @date 9/14/17
-**/
+
 static bool initialized = false;
 
 void updateMotors(){
@@ -54,11 +37,8 @@ void init_slew(){
   initialized = true;
 }
 
-/**
-* @brief Deinitializes the slew rate controller and frees memory.
-* @author Chris Jerrett
-* @date 9/14/17
-**/
+
+
 void deinitslew(){
   free(motors_set_speeds);
   taskDelete(slew);
