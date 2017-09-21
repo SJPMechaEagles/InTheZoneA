cfca8124cbd80b227f6e21f79b888c382ef21c7f
Chris Jerrett <CJerrett18@stjohnsprep.org>
2017-09-21 16:27:32 -0400

    files

--- include/lifter.h
+++ include/lifter.h
@@ -1,6 +1,10 @@
 #ifndef _LIFTER_H_
 #define _LIFTER_H_
 
+#include <API.h>
+#include "motor_ports.h"
+#include "slew.h"
+
 void set_lifter_motors(const char);
 
 void set_lifter_pos(int pos);
--- src/lifter.c
+++ src/lifter.c
@@ -1,7 +1,10 @@
 #include "lifter.h"
 
-void set_lifter_motors(const char) {
-
+void set_lifter_motors(const char v) {
+  set_motor_slew(MOTOR_LIFT_BOTTOM_RIGHT, v);
+  set_motor_slew(MOTOR_LIFT_BOTTOM_LEFT, -v);
+  set_motor_slew(MOTOR_LIFT_TOP_RIGHT, v);
+  set_motor_slew(MOTOR_LIFT_TOP_LEFT, -v);
 }
 
 void set_lifter_pos(int pos) {}
