9b6f171b289006aec7dabd3c585c9d4ea8ab16c5
cdesimone18 <cdesimone18@stjohnsprep.org>
2017-11-03 15:13:26 -0400

    Added pid controller for claw

--- include/claw.h
+++ include/claw.h
@@ -7,6 +7,8 @@
 #include "motor_ports.h"
 
 void update_claw();
+void set_claw_motor(const char v);
+int getClawTicks();
 void open_claw();
 void close_claw();
 
--- src/claw.c
+++ src/claw.c
@@ -2,32 +2,42 @@
 #include "sensor_ports.h"
 
 void update_claw() {
-  static bool pressed_last = true;
+  static bool changed = true;
   static int target = 0;
-
-  if(joystickGetDigital(MASTER, 8, JOY_RIGHT)) {
+  if(joystickGetDigital(MASTER, 8, JOY_RIGHT)){
+    changed = true;
+    target = getClawTicks();
     close_claw();
-  } else if(joystickGetDigital(MASTER, 8, JOY_LEFT)) {
-    open_claw();
-  } else {
-    set_motor_immediate(CLAW_MOTOR, 0);
-    return;
-    int current_pot = analogRead(CLAW_POT);
-    static int last = 0;
-    if(!pressed_last) {
-      target = current_pot;
-      last = current_pot;
-    }
-    static int integral = 0;
-    int p = current_pot - target;
-    int d = current_pot - last;
-    integral+=p;
-
-    int motor_value = p/100 + d/1000 + integral/10000;
-    last = current_pot;
-    //printf("P: %d I: %d D: %d\n", p/100, d/1000, integral/10000);
-    set_motor_immediate(CLAW_MOTOR, -motor_value);
   }
+  else if(joystickGetDigital(MASTER, 8, JOY_LEFT)) {
+    changed = true;
+    target = getClawTicks();
+    open_claw();
+  }
+  else {
+    static int i = 0;
+    if(changed) {
+      target = getClawTicks();
+      i = 0;
+    }
+    int p = target - getClawTicks();
+    i += p;
+    int d = target - getClawTicks();
+    int motorVal = -p/10 - d/100 -i/400;
+    //printf("%d\n", motorVal);
+
+    set_claw_motor(motorVal);
+    changed = false;
+  }
+}
+
+
+void set_claw_motor(const char v){
+  set_motor_slew(CLAW_MOTOR, v);
+}
+
+int getClawTicks(){
+  return analogReadCalibrated(CLAW_POT);
 }
 
 void open_claw() {
