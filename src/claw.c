bf12a8d51e9ecd9ab4b8bdfe176878e7e846c92b
cdesimone18 <cdesimone18@stjohnsprep.org>
2017-11-07 15:49:20 -0500

    Made Claw a toggle

--- src/claw.c
+++ src/claw.c
@@ -1,15 +1,31 @@
 #include "claw.h"
 
+/*void update_claw(){
+  static bool closed = false;
+  if(closed){
+    if((analogRead(CLAW_POT) - CLAW_CLOSE) > 20){
+      close_claw();
+    }
+    else set_claw_motor(0);
+  }
+  else if(!closed){
+    if((analogRead(CLAW_POT) - CLAW_OPEN) > 20){
+      open_claw();
+    }
+    else set_claw_motor(0);
+  }
+}*/
+
 void update_claw() {
   static bool changed = true;
   static int target = 0;
   if(joystickGetDigital(CLAW_OPEN)){
     changed = true;
-    target = getClawTicks();
+    target = CLAW_OPEN;
     close_claw();
   } else if(joystickGetDigital(CLAW_CLOSE)) {
     changed = true;
-    target = getClawTicks();
+    target = CLAW_CLOSE;
     open_claw();
   } else {
     static int i = 0;
